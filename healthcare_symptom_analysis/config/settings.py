7405b759e764d4e5e1a166c95f4dc9e887811a68
khushbu <khushbukumari204566@gmail.com>
2026-07-14 01:23:40 +0530

    allowed host

--- healthcare_symptom_analysis/config/settings.py
+++ healthcare_symptom_analysis/config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ["healthcare-backend-khushbu-egfugbakgba4eway.centralus-01.azurewebsites.net"]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
